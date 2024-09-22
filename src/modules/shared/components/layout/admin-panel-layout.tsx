7ac79f9e0534efef94679edb0cf0ba32b99bb29e
kztera <116504460+kztera@users.noreply.github.com>
2024-09-23 00:15:03 +0700

    update: mở rộng sidebar > 18rem

--- src/modules/shared/components/layout/admin-panel-layout.tsx
+++ src/modules/shared/components/layout/admin-panel-layout.tsx
@@ -19,7 +19,7 @@ export default function AdminPanelLayout({
       <main
         className={cn(
           'min-h-[calc(100vh_-_56px)] bg-zinc-50 dark:bg-zinc-900 transition-[margin-left] ease-in-out duration-300',
-          sidebar?.isOpen === false ? 'lg:ml-[90px]' : 'lg:ml-60',
+          sidebar?.isOpen === false ? 'lg:ml-[90px]' : 'lg:ml-72',
         )}
       >
         {children}
@@ -27,7 +27,7 @@ export default function AdminPanelLayout({
       <footer
         className={cn(
           'transition-[margin-left] ease-in-out duration-300',
-          sidebar?.isOpen === false ? 'lg:ml-[90px]' : 'lg:ml-60',
+          sidebar?.isOpen === false ? 'lg:ml-[90px]' : 'lg:ml-72',
         )}
       >
         <Footer />
--- src/modules/shared/components/layout/sidebar.tsx
+++ src/modules/shared/components/layout/sidebar.tsx
@@ -19,7 +19,7 @@ export function Sidebar() {
     <aside
       className={cn(
         'fixed top-0 left-0 z-20 h-screen -translate-x-full lg:translate-x-0 transition-[width] ease-in-out duration-300',
-        sidebar?.isOpen === false ? 'w-[90px]' : 'w-60',
+        sidebar?.isOpen === false ? 'w-[90px]' : 'w-72',
       )}
     >
       <SidebarToggle isOpen={sidebar?.isOpen} setIsOpen={sidebar?.setIsOpen} />
