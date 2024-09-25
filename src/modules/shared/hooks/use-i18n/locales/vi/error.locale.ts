df2ba0dd2103fc4a069a74dbfe28c73404285f57
kztera <116504460+kztera@users.noreply.github.com>
2024-09-25 12:07:29 +0700

    update: localization for authentication and validation messages

--- src/modules/shared/hooks/use-i18n/locales/vi/error.locale.ts
+++ src/modules/shared/hooks/use-i18n/locales/vi/error.locale.ts
@@ -1,5 +1,6 @@
 export const errorLocale = {
   er_401: 'Vui lòng đăng nhập để tiếp tục',
+  UNKNOWN_ERROR: 'Có lỗi xảy ra, vui lòng thử lại',
 } as const;
 
 export type ErrorLocale = keyof typeof errorLocale;
