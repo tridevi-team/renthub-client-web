89c9fca956122be301ecdc1e83a71a1289a7f715
kztera <116504460+kztera@users.noreply.github.com>
2024-10-12 11:29:12 +0700

    feat: add environment variables for development, production, and staging; update package dependencies; enhance main CSS styles; introduce Tailwind indicator component

--- src/modules/shared/constants/env.constant.ts
+++ src/modules/shared/constants/env.constant.ts
@@ -3,6 +3,10 @@ import { z } from 'zod';
 export const envSchema = z.object({
   VITE_APP_TITLE: z.string(),
   VITE_API_BASE_URL: z.string().url(),
+  NODE_ENV: z
+    .literal('development')
+    .or(z.literal('production'))
+    .or(z.literal('staging')),
 });
 
 export const env = (() => {
@@ -12,9 +16,11 @@ export const env = (() => {
   const apiBaseUrl = envSchema.shape.VITE_API_BASE_URL.parse(
     import.meta.env.VITE_API_BASE_URL,
   );
+  const NODE_ENV = envSchema.shape.NODE_ENV.parse(import.meta.env.NODE_ENV);
 
   return {
     appTitle,
     apiBaseUrl,
+    NODE_ENV,
   };
 })();
