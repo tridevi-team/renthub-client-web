86544a6846f730fee63f8506ad09bf348f55ae49
kztera <116504460+kztera@users.noreply.github.com>
2024-10-12 14:22:10 +0700

    feat: update environment variable naming to VITE_NODE_ENV; remove unused components

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -7,6 +7,7 @@ interface ImportMetaEnv {
   // prefixed with "VITE_" -> exposed to our Vite-processed code
   readonly VITE_APP_TITLE: string | undefined;
   readonly VITE_API_BASE_URL: string | undefined;
+  readonly VITE_NODE_ENV: 'development' | 'production' | 'test';
 }
 
 interface ImportMeta {
