cc6e1092f6012ff995ab1c15687a2ef91f7e5294
kztera <116504460+kztera@users.noreply.github.com>
2024-10-26 18:29:44 +0700

    refactor: remove close icon from dialog and adjust input height

--- src/modules/shared/components/ui/input.tsx
+++ src/modules/shared/components/ui/input.tsx
@@ -34,7 +34,7 @@ const _Input = React.forwardRef<HTMLInputElement, InputProps>(
         )}
         <Input
           className={twMerge(
-            'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:font-medium file:text-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
+            'flex h-9 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:font-medium file:text-sm placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
             prefixElement && 'pl-14',
             suffixElement && 'pr-8',
             className,
