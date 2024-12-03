4d2b902ea15bc790e39ff7c617d8ee0e93f12e25
kztera <116504460+kztera@users.noreply.github.com>
2024-12-03 22:53:21 +0700

    feat: add Radix UI switch component and update package.json

--- src/modules/shared/components/ui/switch.tsx
+++ src/modules/shared/components/ui/switch.tsx
@@ -1,25 +1,27 @@
-import { Switch, type SwitchProps } from 'react-aria-components';
-import { twMerge } from 'tailwind-merge';
+import * as SwitchPrimitives from '@radix-ui/react-switch';
+import * as React from 'react';
 
-const _Switch = ({ children, className, ...props }: SwitchProps) => (
-  <Switch
-    className={(values) =>
-      twMerge(
-        'group inline-flex items-center gap-2 font-medium text-sm leading-none data-[disabled]:cursor-not-allowed data-[disabled]:opacity-70',
-        typeof className === 'function' ? className(values) : className,
-      )
-    }
-    {...props}
-  >
-    {(values) => (
-      <>
-        <div className="h-[24px] w-[44px] shrink-0 cursor-pointer rounded-full border-2 border-transparent bg-input transition-colors group-data-[selected]:bg-primary group-data-[disabled]:opacity-50 group-data-[focus-visible]:outline-none group-data-[focus-visible]:ring-1 group-data-[focus-visible]:ring-ring group-data-[focus-visible]:ring-offset-0 group-data-[focus-visible]:ring-offset-background">
-          <div className="pointer-events-none block h-5 w-5 translate-x-0 rounded-full bg-background shadow-lg ring-0 transition-transform group-data-[selected]:translate-x-5" />
-        </div>
-        {typeof children === 'function' ? children(values) : children}
-      </>
+import { cn } from '@app/lib/utils';
+
+const Switch = React.forwardRef<
+  React.ElementRef<typeof SwitchPrimitives.Root>,
+  React.ComponentPropsWithoutRef<typeof SwitchPrimitives.Root>
+>(({ className, ...props }, ref) => (
+  <SwitchPrimitives.Root
+    className={cn(
+      'peer inline-flex h-5 w-9 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input',
+      className,
     )}
-  </Switch>
-);
+    {...props}
+    ref={ref}
+  >
+    <SwitchPrimitives.Thumb
+      className={cn(
+        'pointer-events-none block h-4 w-4 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0',
+      )}
+    />
+  </SwitchPrimitives.Root>
+));
+Switch.displayName = SwitchPrimitives.Root.displayName;
 
-export { _Switch as Switch };
+export { Switch };
